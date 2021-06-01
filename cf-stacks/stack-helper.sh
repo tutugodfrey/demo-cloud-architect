45fcd2602fd586ffea373981fa558e927345d6e2
tutugodfrey <godfrey_tutu@yahoo.com>
2021-06-01 22:58:06 +0100

    Add Load Balancer
    - Add CloudFormation template to create Security Groups
    - Add CloudFormation template to create Load Balancer
    - Update Nat Gateway template
    - Update AppSubnet Routes with Internet gateway instead of NAT Gateway

--- cf-stacks/stack-helper.sh
+++ cf-stacks/stack-helper.sh
@@ -12,6 +12,16 @@ aws cloudformation create-stack --stack-name dca-nat-gateway-stack --template-bo
 # Update the nat gateway stack
 aws cloudformation update-stack --stack-name dca-nat-gateway-stack --template-body file://cf-stacks/nat-gateway.yml
 
+# Create Security group stack
+aws cloudformation create-stack --stack-name dca-security-sg --template-body file://cf-stacks/security-groups.yml
+
+# Create the Load Balancer stack
+aws cloudformation create-stack --stack-name dca-loadbalancer --template-body file://cf-stacks/load-balancer.yml
+
+# Update the Load Balancer Stack
+aws cloudformation update-stack --stack-name dca-loadbalancer --template-body file://cf-stacks/load-balancer.yml
+
 # Deleting stacks
 aws cloudformation delete-stack --stack-name cda-pvc-stack # VPC Stack
 aws cloudformation delete-stack --stack-name dca-nat-gateway-stack # Nat gateway stack
+aws cloudformation delete-stack --stack-name dca-loadbalancer # Delete the load balancer stack
